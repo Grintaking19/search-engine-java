9e67c68bf63c6fdbef0eeac6eaeeeb9516998042
karimyasser107 <karimyasser34@gmail.com>
2023-05-16 02:10:58 +0300

    crawler downloads html docs, store urls in online d

--- src/main/java/Crawler/MongoDBClass.java
+++ src/main/java/Crawler/MongoDBClass.java
@@ -2,11 +2,44 @@ package Crawler;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+//import javax.swing.text.Document;
 
 public class MongoDBClass {
-    public static void main(String args[]) {
+    String uri;
+    MongoClientURI clientURI;
+    MongoClient mongoClient;
+    MongoDatabase mongoDatabase;
+    MongoCollection collection;
+    public MongoDBClass() {
         String uri="mongodb+srv://karimyasser34:3uRq0QAZoqUbUPzN@cluster0.8ikdynj.mongodb.net/";
-        MongoClientURI clientURI=new MongoClientURI(uri);
-        MongoClient mongoClient=new MongoClient(clientURI);
+        this.clientURI=new MongoClientURI(uri);
+        this.mongoClient=new MongoClient(clientURI);
+        //connect to db
+        this.mongoDatabase=mongoClient.getDatabase("Crawler");
+        //get access to collection
+        this.collection=mongoDatabase.getCollection(("htmlDocs"));
     }
+    //function that insert a url, the file path into the database
+    public void insertRecord(String link,String filePath){
+        Document doc=new Document("url",link);
+        doc.append("filePath",filePath);
+        collection.insertOne(doc);
+
+    }
+
+
+//    public static void main(String args[]) {
+//
+//
+//        Document search=new Document("url","fge");
+//        if (search!=null) {
+//            Bson updateUrl=new Document("visited",true);
+//            Bson updateOperation=new Document("$set",updateUrl);
+//            collection.updateOne(search,updateOperation);
+//        }
+//    }
 }
--- src/main/java/Crawler/crawler.java
+++ src/main/java/Crawler/crawler.java
@@ -5,39 +5,45 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-
 // we must implement runnable as this is a multithreaded web crawler
 public class crawler implements Runnable{
     //max depth of crawling
 //    private static final int MAX_DEPTH=3;
-    private static final int MAX_NUMBER_OF_LINKS=6000;
+    MongoDBClass dataBaseCrawler;
+    private static final int MAX_NUMBER_OF_LINKS=600;
     private Object countUrls;
     private Queue<String> urlsQueue = new LinkedList<>();
     private int id;
-    private ArrayList<String> visitedUrls=new ArrayList<String>();
+    private ArrayList<String> visitedUrls=new ArrayList<>();
     //constructor
-    public crawler(ArrayList<String> visitedUrls, String link, int num, Object countUrls){
+    public crawler(MongoDBClass dataBaseCrawler,ArrayList<String> visitedUrls, String link, int num, Object countUrls){
+        this.dataBaseCrawler=dataBaseCrawler;
         this.urlsQueue.add(link);
         this.visitedUrls=visitedUrls;
         this.id=num;
         this.countUrls=countUrls;
 
+
     }
     //as we implement runnable we need to override function run
     @Override
     public void run() {
-        crawl(1);
+        crawl();
     }
 
-    private void crawl(int level){
+    private void crawl(){
+        String popedUrl;
+        Document doc;
+        String next_link;
+        String filePath;
         //while the queue of urls still have urls then crawl the top of the queue
         while(! this.urlsQueue.isEmpty()){
             //check if we reached the MAX_NUMBER_OF_LINKS crawled
@@ -47,15 +53,25 @@ public class crawler implements Runnable{
                     break;
                 }
             }
-            String popedUrl=urlsQueue.remove();
+            popedUrl=urlsQueue.remove();
 
-            //System.out.println("level= "+level+"\n");
-            Document doc=request(popedUrl);
+            doc=request(popedUrl);
             if(doc !=null){
+                //download html page
+                filePath=downloadHtmlDoc(popedUrl);
+                if(filePath !=""){
+                    //we want to add this popedurl to the collection of htmlDocs in the database
+                    synchronized (this.dataBaseCrawler){
+                        this.dataBaseCrawler.insertRecord(popedUrl,filePath);
+                    }
+                }
+                else{
+                    System.out.println("Error empty link!");
+                }
                 //add the popped url to visitedUrls list
                 this.addUrlToVisitedUrls(popedUrl);
                 for(Element link: doc.select("a[href]")) {
-                    String next_link=link.absUrl("href");
+                    next_link=link.absUrl("href");
                     //check if this link is visited before inserting in urlsQueue
                     if(this.visitedUrls.contains(next_link)==false){
                         //crawl(level+1,next_link);
@@ -65,6 +81,42 @@ public class crawler implements Runnable{
             }
 
         }
+    }
+    public static String removeSpecialCharacters(String str) {
+        // Using regex to replace all special characters with an empty string
+        return str.replaceAll("[^a-zA-Z0-9 ]", "");
+    }
+    public static String trimLastNCharacters(String str) {
+        if (230 >= str.length()) {
+            return str;
+        }
+        return str.substring(str.length() - 230);
+    }
+    public static String downloadHtmlDoc(String link) {
+        try {
+            String filePath = removeSpecialCharacters(trimLastNCharacters(link)) + ".html";
+            URL url = new URL(link);
+            BufferedReader readHtml = new BufferedReader(new InputStreamReader(url.openStream()));
+            BufferedWriter writeHtml = new BufferedWriter(new FileWriter("D:\\CUFE\\Spring 2023\\apt\\project\\htmlDocs\\" + filePath));
+
+            // read each line from stream till end
+            String docLine;
+
+            while ((docLine = readHtml.readLine()) != null) {
+                //write line to
+                writeHtml.write(docLine);
+            }
+            readHtml.close();
+            writeHtml.close();
+            return filePath;
+        }
+        // Exceptions
+        catch (MalformedURLException mue) {
+            System.out.println("MalformedURL Exception occurred");
+        } catch (IOException ie) {
+            System.out.println("IOException occurred");
+        }
+        return"";
     }
     //this function takes a url and lock the visitedUrls list to forbid race condition
     private  void addUrlToVisitedUrls(String url){
@@ -118,18 +170,17 @@ public class crawler implements Runnable{
         return seedUrlsList;
     }
 
-    class counUrls{
-
-    }
-
 
     public static void main(String[] args) throws InterruptedException {
+        //create an instance of the database
+        MongoDBClass dataBaseCrawler=new MongoDBClass();
+
         //create threads list
         ArrayList<Thread> botsList=new ArrayList<>();
         //create visitedUrls list
-        ArrayList<String> visitedUrls=new ArrayList<String>();
+        ArrayList<String> visitedUrls=new ArrayList<>();
         //create seedUrls list
-        ArrayList<String> seedUrls=new ArrayList<String>();
+        ArrayList<String> seedUrls;
         //create a counter to count number of links crawled
         Object countUrls=new Object();
 
@@ -142,7 +193,7 @@ public class crawler implements Runnable{
         }
         //loop on numThreads and create a new thread
         for (int i = 0; i < numThreads; i++) {
-            Thread threadInst = new Thread(new crawler( visitedUrls, seedUrls.get(i),  i,countUrls));
+            Thread threadInst = new Thread(new crawler(dataBaseCrawler, visitedUrls, seedUrls.get(i),  i,countUrls));
             //set id of thread to be its index
             String id = Integer.toString(i);
             threadInst.setName(id);
