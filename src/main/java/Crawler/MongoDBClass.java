564f2e3b84179939656a420c91131fa0d97e187e
karimyasser107 <karimyasser34@gmail.com>
2023-05-17 05:04:52 +0300

    robot.txt added
    stop recrawl and continue crawl not fulfilled

--- src/main/java/Crawler/MongoDBClass.java
+++ src/main/java/Crawler/MongoDBClass.java
@@ -2,34 +2,89 @@ package Crawler;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static com.mongodb.client.model.Filters.eq;
 //import javax.swing.text.Document;
 
 public class MongoDBClass {
-    String uri;
+    String connectionString;
     MongoClientURI clientURI;
     MongoClient mongoClient;
     MongoDatabase mongoDatabase;
-    MongoCollection collection;
+    MongoCollection htmlDocsCollection;
+    MongoCollection urlsQueueCollection;
     public MongoDBClass() {
-        String uri="mongodb+srv://karimyasser34:3uRq0QAZoqUbUPzN@cluster0.8ikdynj.mongodb.net/";
-        this.clientURI=new MongoClientURI(uri);
+        this.connectionString="mongodb+srv://karimyasser34:3uRq0QAZoqUbUPzN@cluster0.8ikdynj.mongodb.net/";
+        this.clientURI=new MongoClientURI(connectionString);
         this.mongoClient=new MongoClient(clientURI);
         //connect to db
         this.mongoDatabase=mongoClient.getDatabase("Crawler");
-        //get access to collection
-        this.collection=mongoDatabase.getCollection(("htmlDocs"));
+        //get access to collections
+        this.htmlDocsCollection=mongoDatabase.getCollection(("htmlDocs"));
+        this.urlsQueueCollection=mongoDatabase.getCollection(("unvisitedUrlsQueue"));
     }
     //function that insert a url, the file path into the database
-    public void insertRecord(String link,String filePath){
+    public void insertVisitedUrlRecord(String link, String filePath){
         Document doc=new Document("url",link);
         doc.append("filePath",filePath);
-        collection.insertOne(doc);
+        htmlDocsCollection.insertOne(doc);
 
     }
+    //insert at end of the collection ( queue )
+    public void insertUnvisitedUrlInQueue(String link){
+        Document doc=new Document("url",link);
+        urlsQueueCollection.insertOne(doc);
+    }
+
+    //function get the arrayList of visitedUrls from the collection "htmlDocsCollection"
+    public ArrayList<String> getVisitedUrlsFromCollection() {
+        MongoCollection<Document> visitedUrlsCollection = this.mongoDatabase.getCollection("htmlDocs");
+        // Find all documents in the collection and add the string value to an ArrayList
+        ArrayList<String> visitedUrlsList = new ArrayList<>();
+        String url;
+        for (Document doc : visitedUrlsCollection.find()) {
+             url= doc.getString("url");
+            visitedUrlsList.add(url);
+        }
+        return visitedUrlsList;
+    }
+
+    //function get the arrayList of visitedUrls from the collection "urlsQueueCollection"
+    public Queue<String> getUrlsQueueFromCollection() {
+        MongoCollection<Document> visitedUrlsCollection = this.mongoDatabase.getCollection("unvisitedUrlsQueue");
+        // Find all documents in the collection and add the string value to an ArrayList
+        Queue<String> unvisitedUrlsQueue = new LinkedList<>();
+        String url;
+        for (Document doc : visitedUrlsCollection.find()) {
+            url= doc.getString("url");
+            unvisitedUrlsQueue.add(url);
+        }
+        return unvisitedUrlsQueue;
+    }
+
+    //function delete to delete url from collection "urlsQueueCollection"
+    public void deleteUrlFromUrlsQueueCollection(String link){
+        // Get the "urlsQueueCollection" collection
+        MongoCollection<Document> collection = this.mongoDatabase.getCollection("urlsQueueCollection");
+
+        try {
+            // Delete a document where the "url" attribute has the value "link"
+            System.out.println("to be deleted from db    "+link);
+            collection.deleteMany(eq("url", link));
+        } catch (MongoException e) {
+            // Handle the exception appropriately, such as logging the error message
+            System.err.println("Error deleting document from collection: " + e.getMessage());
+        }
+    }
+
 
 
 //    public static void main(String args[]) {
